c72849fcae24f14d846f5acf96ea7cc318bf50b1
jonny <32085184+jonnyjohnson1@users.noreply.github.com>
2026-06-01 15:34:56 -0500

    Bump topos-node version to 0.1.4.

--- topos/__version__.py
+++ topos/__version__.py
@@ -1,6 +1,6 @@
 """Topos Control Plane Version Information."""
 
-__version__ = "0.1.3"
-__version_info__ = (0, 1, 3)
+__version__ = "0.1.4"
+__version_info__ = (0, 1, 4)
 
 __all__ = ["__version__", "__version_info__"]
